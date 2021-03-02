81bbf2dce66a0dd96c8d932c1fb3307fdedcae52
jhonmeche <jhonmeche96@gmail.com>
2021-03-02 00:41:56 -0500

    clean project

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import React from 'react'
 function App() {
   return (
     <div className="container mt-5">
-      <h1>Tareas</h1>
+      <h1>Tareas!</h1>
       <hr/>
     </div>
-  );
+  )
 }
 
 export default App;
